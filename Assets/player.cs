895168025ba71971be31dc678ba6d3c457a76e07
YeraYarey <yeray.arnal.cano.alu@iesjulioverne.es>
2023-03-15 20:36:45 +0100

    movimiento y salto del jugador

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    
+    private Rigidbody2D rb2D;
+
+    //MOVIMIENTO
+    private float movimientoHorizontal = 0f;
+    private float velocidadMovimiento = 500f; 
+    private float suavizadoMovimiento = 0.3f;
+
+    private Vector3 velocidad = Vector3.zero;
+
+    private bool mirandoDerecha = true;
+
+    //SALTO
+    public float fuerzaSalto;
+    public LayerMask queEsSuelo;
+    public Transform controladorSuelo;
+    public Vector3 dimensionesCaja;
+    private bool enSuelo;
+    private bool salto=false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movimientoHorizontal= Input.GetAxisRaw("Horizontal") * velocidadMovimiento;
+
+        if(Input.GetButtonDown("Jump")){
+            salto = true;
+        }
     }
+
+    private void Mover(float mover, bool saltar){
+        Vector3 velocidadObjetivo = new Vector2(mover, rb2D.velocity.y);
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, velocidadObjetivo, ref velocidad, suavizadoMovimiento);
+
+        if(mover > 0 && !mirandoDerecha){
+            Girar();
+        }else if(mover < 0 && mirandoDerecha){
+            Girar();
+        }
+
+        if(enSuelo && saltar){
+            enSuelo = false;
+            rb2D.AddForce(new Vector2(0f, fuerzaSalto));
+        }
+    }
+
+    private void Girar(){
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+
+    }
+
+    private void FixedUpdate(){
+        Mover(movimientoHorizontal * Time.fixedDeltaTime, salto);
+
+        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
+
+        salto = false;
+    }
+
 }
